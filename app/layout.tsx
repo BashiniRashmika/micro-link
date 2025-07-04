66367c3fd65e877c77013dd8f409e669c065874d
Bashini Rashmika <bashinirashmika2001@gmail.com>
2025-07-04 23:36:31 +0530

    dataDog RUM init

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,33 +29,29 @@ export default function RootLayout({
         </ThemeProvider>
         
         {/* Datadog RUM Script */}
-        <Script id="datadog-rum" strategy="afterInteractive">
-          {`
-            (function(h,o,u,n,d) {
-              h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
-              d=o.createElement(u);d.async=1;d.src=n
-              n=o.getElementsByTagName(u)[0];n.parentNode.insertBefore(d,n)
-            })(window,document,'script','https://www.datadoghq-browser-agent.com/datadog-rum.js','DD_RUM')
-            DD_RUM.onReady(function() {
-              DD_RUM.init({
-                applicationId: '${process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID}',
-                clientToken: '${process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN}',
-                site: 'datadoghq.com',
-                service: 'micro-link',
-                env: '${process.env.NODE_ENV || 'development'}',
-                version: '1.0.0',
-                sampleRate: 100,
-                trackInteractions: true,
-                defaultPrivacyLevel: 'mask-user-input'
-              }),
-              DD_RUM.setUser({
-                id: 'user-id',
-               name: 'user-name',
-               email: 'user-email'
-              })
-            })
-          `}
-        </Script>
+        <script>
+          (function(h,o,u,n,d) {
+            h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
+            d=o.createElement(u);d.async=1;d.src=n
+            n=o.getElementsByTagName(u)[0];n.parentNode.insertBefore(d,n)
+          })(window,document,'script','https://www.datadoghq-browser-agent.com/us5/v6/datadog-rum.js','DD_RUM')
+          window.DD_RUM.onReady(function() {
+            window.DD_RUM.init({
+              clientToken: 'pub03071b05f8a072412f3f0f3ac8ab552c',
+              applicationId: '67f4dacc-2c15-485d-8524-ccc19cabed49',
+              // `site` refers to the Datadog site parameter of your organization
+              // see https://docs.datadoghq.com/getting_started/site/
+              site: 'us5.datadoghq.com',
+              service: 'micro-link',
+              env: 'production',
+              // Specify a version number to identify the deployed version of your application in Datadog
+              // version: '1.0.0',
+              sessionSampleRate: 100,
+              sessionReplaySampleRate: 20,
+              defaultPrivacyLevel: 'mask-user-input',
+            });
+          })
+        </script>
       </body>
     </html>
   )
